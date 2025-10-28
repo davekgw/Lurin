c9ef5013a106eb5d3d0bba1f6ed047c2c1d9aac5
Smiley~ <45153558+davekgw@users.noreply.github.com>
2025-10-28 15:10:42 +0700

    Update configuration values in config.js

--- settings/config.js
+++ settings/config.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const config = {
     owner: "-",
     botNumber: "-",
-    setPair: "K1UU1212",
+    setPair: "DAVEKGOW",
     thumbUrl: "https://github.com/kiuur.png",
     session: "sessions",
     status: {
@@ -18,20 +18,20 @@ const config = {
         private: "this is specifically for private chat"
     },
     settings: {
-        title: "w-shennmine",
-        packname: 'laurine-wabot',
-        description: "this script was created by KyuuRzy",
-        author: 'https://www.kyuurzy.tech',
-        footer: "shenń, yes 1437`"
+        title: "Dev_",
+        packname: 'self-wabot',
+        description: "Smiley_",
+        author: 'https://github.com/davekgw',
+        footer: "SelfBot"
     },
     newsletter: {
-        name: "kyuurzy-wb",
-        id: "120363297591152843@newsletter"
+        name: "Smiley",
+        id: "123"
     },
     socialMedia: {
-        YouTube: "https://youtube.com/@kyuurzy",
-        GitHub: "https://github.com/kiuur",
-        Telegram: "https://t.me/kiuurmine",
+        YouTube: "-",
+        GitHub: "https://github.com/davekgw",
+        Telegram: "https://t.me/davekgw",
         ChannelWA: "https://whatsapp.com/channel/0029Vaeqym9IHphHwvXk9k1s"
     }
 }
